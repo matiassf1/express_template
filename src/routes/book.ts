7a778eaeb61a33b7492325f9b501bd5cacbdd9c6
Matias Sfer <matias0dveloper@gmail.com>
2024-03-25 09:53:06 -0300

    Add functionality to http verbs

--- src/routes/book.ts
+++ src/routes/book.ts
@@ -1,23 +1,70 @@
 import { Router } from "express";
+import { readJson } from "../utils";
+import { validateBook, validatePartialBook } from "../schemas/book";
+
+const books = readJson('../book.json')
 
 export const bookRouter = Router();
 
 bookRouter.get('/', (req, res) => {
-
+    return res.json(books);
 })
 
 bookRouter.get('/:id', (req, res) => {
+    const { id } = req.query;
+    const book = books.find(book => book.id === id);
+
+    if (!book) return res.status(404).json({ message: `Book with ${id} ID not found.` })
+
+    return res.json(book);
 
 })
 
 bookRouter.post('/', (req, res) => {
+    const result = validateBook(req.body);
 
+    if (!result.success) {
+        return res.status(402).json({ error: JSON.parse(result.error.message) });
+    }
+
+    const newBook = books.push(result.data);
+
+    res.status(201).json(newBook);
 })
 
-bookRouter.patch('/', (req, res) => {
+bookRouter.patch('/:id', (req, res) => {
+    const { id } = req.query;
+    const result = validatePartialBook(req.body);
 
+    if (!result.success) {
+        return res.status(400).json({ error: JSON.parse(result.error.message) });
+    }
+
+    const bookIndex = books.findIndex(book => book.id === id);
+    if (bookIndex === -1) {
+        return res.status(401).json({ message: 'Book not found' });
+    }
+
+    const updateBook = {
+        ...books[bookIndex],
+        ...result.data
+    };
+
+    books[bookIndex] = updateBook;
+
+    return res.json(updateBook);
 })
 
-bookRouter.delete('/', (req, res) => {
+bookRouter.delete('/:id', (req, res) => {
+    const { id } = req.query;
 
+    const bookIndex = books.findIndex((book) => book.id === id);
+
+    if (bookIndex === -1) {
+        return res.status(401).json({ message: 'Book not found' });
+    }
+
+    books.splice(bookIndex, 1);
+
+    return res.json(books[bookIndex]);
 })
