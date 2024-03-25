c1e43bd9abdb56b18f8442736e534964257ee5aa
Matias Sfer <matias0dveloper@gmail.com>
2024-03-25 09:12:26 -0300

    Create class models

---
+++ src/model/mongodb/book.ts
@@ -0,0 +1,17 @@
+export class bookModel {
+    static async getAll() {
+
+    }
+
+    static async getById() {
+
+    }
+
+    static async deleteById() {
+
+    }
+
+    static async create() {
+
+    }
+}
