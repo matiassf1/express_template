caaec1978874a1e7dfa576420c353652be614532
Matias Sfer <matias0dveloper@gmail.com>
2024-03-25 08:37:00 -0300

    Create routes

---
+++ src/routes/books.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+
+export const booksRouter = Router();
+
+booksRouter.get('/', (req, res) => {
+
+})
+
+booksRouter.get('/:id', (req, res) => {
+
+})
+
+booksRouter.post('/', (req, res) => {
+
+})
+
+booksRouter.patch('/', (req, res) => {
+
+})
+
+booksRouter.delete('/', (req, res) => {
+
+})
