a6b167ab02318ee5a0e41ad33902c92ff3238b6f
Matias Sfer <matias0dveloper@gmail.com>
2024-03-25 08:37:11 -0300

    Create routes

---
+++ src/routes/authors.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+
+export const authorsRouter = Router();
+
+authorsRouter.get('/', (req, res) => {
+
+})
+
+authorsRouter.get('/:id', (req, res) => {
+
+})
+
+authorsRouter.post('/', (req, res) => {
+
+})
+
+authorsRouter.patch('/', (req, res) => {
+
+})
+
+authorsRouter.delete('/', (req, res) => {
+
+})
