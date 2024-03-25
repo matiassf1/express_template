0827093ea8b82c9078894ddde935a81056a49e7f
Matias Sfer <matias0dveloper@gmail.com>
2024-03-25 08:55:13 -0300

    Implement json and cors middlewares

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import express, { json } from 'express'
 import { booksRouter } from './routes/books'
 import { authorsRouter } from './routes/authors';
+import { corsMiddleware } from './middlewares/cors';
 
 const app = express()
 const port = process.env.PORT ?? 3000
+
+app.use(json());
+app.use(corsMiddleware());
 
 app.use('/books', booksRouter);
 app.use('/authors', authorsRouter);
