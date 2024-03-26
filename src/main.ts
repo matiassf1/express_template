0182af1910809b82552c8f6ec53a2a203caaa300
Matias Sfer <matias0dveloper@gmail.com>
2024-03-26 11:30:32 -0300

    Implement new book router

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { bookRouter } from "./routes/book";
 import cors from "cors";
 import { uri } from "../config";
 import { Database } from "./database/MongoDb";
+import { BookController } from "./controllers/book";
 
 const app = express();
 const port = process.env.PORT ?? 3000
@@ -14,10 +15,11 @@ app.use(async (req, res, next) => {
     await db.connect();
     next();
 });
+
 app.use(express.json());
 app.use(cors());
 
-app.use('/books', bookRouter);
+app.use('/books', bookRouter(new BookController(db)));
 app.use('/authors', authorRouter);
 
 app.use(async (req, res, next) => {
