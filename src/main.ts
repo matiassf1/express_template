19d7d8b41a3bced72f4a354b830f13e86074b4e9
Matias Sfer <matias0dveloper@gmail.com>
2024-03-26 12:47:34 -0300

    Update bookController instance

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import cors from "cors";
 import { uri } from "../config";
 import { Database } from "./database/MongoDb";
 import { BookController } from "./controllers/book";
+import { BookMongoModel } from "./model/book";
 
 const app = express();
 const port = process.env.PORT ?? 3000
 
 const db = new Database(uri, 'library');
+const bookMongoModel = new BookMongoModel(db);
 
 app.use(async (req, res, next) => {
     await db.connect();
@@ -19,7 +21,7 @@ app.use(async (req, res, next) => {
 app.use(express.json());
 app.use(cors());
 
-app.use('/books', bookRouter(new BookController(db)));
+app.use('/books', bookRouter(new BookController(bookMongoModel)));
 app.use('/authors', authorRouter);
 
 app.use(async (req, res, next) => {
