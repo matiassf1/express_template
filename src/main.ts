6e45ad854cce736d7b77d2e4b0b8e421897ff113
Matias Sfer <matias0dveloper@gmail.com>
2024-03-26 11:24:04 -0300

    Add midlewares to handle connections

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,26 @@ import express from "express";
 import { authorRouter } from "./routes/author";
 import { bookRouter } from "./routes/book";
 import cors from "cors";
+import { uri } from "../config";
+import { Database } from "./database/MongoDb";
 
 const app = express();
 const port = process.env.PORT ?? 3000
 
+const db = new Database(uri);
+
+app.use(async (req, res, next) => {
+    await db.connect();
+    next();
+});
 app.use(express.json());
 app.use(cors());
 
 app.use('/books', bookRouter);
 app.use('/authors', authorRouter);
+
+app.use(async (req, res, next) => {
+    await db.close();
+    next();
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
